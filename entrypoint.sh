80ddd81877eafd9372eb6a27bf8e8a5c2525d5d6
chen <wszgrcy@gmail.com>
2024-03-22 14:36:16 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ls -lh
 npm i @code-recycle/cli -g
+export LANG="en"
+code-recycle --help
 code-recycle version
 
 I_PATH=$1
 I_CWD=$([ -n "$2" ] && echo -n "--cwd $2" || echo -n "")
 I_DRY_RUN=$([ "$3" == "true" ] && echo -n "--dryRun" || echo -n "")
-env
+
 code-recycle ${I_PATH} ${I_CWD} ${I_DRY_RUN}
